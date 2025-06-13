e2f3cb47ac5175556c1dcf8a0f1ddf93904fb835
alexisalvz3 <alexis.alvz3@gmail.com>
2025-06-12 20:51:44 -0700

    setting up db engine pt. 5

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app.database import SessionLocal, engine, Base
 from app.models import Reservation, NotificationLog, AdminUser  # all your models
 
@@ -6,21 +7,24 @@ Base.metadata.create_all(bind=engine)
 reservations = [
         {
             "name": "kambala",
+            "email": "kambala@gmail.com",
             "phone": "507-2424",
             "table_size": 10,
-            "date_time": "2025-04-02T19:51:11.161000"
+            "date_time": datetime.fromisoformat("2025-04-02T19:51:11.161000")
         },
         {
             "name": "Thomas Shelby",
+            "email": "tommy@gmail.com",
             "phone": "432-4653",
             "table_size": 3,
-            "date_time": "2025-04-03T02:18:15.356000"
+            "date_time": datetime.fromisoformat("2025-04-03T02:18:15.356000")
         },
         {
             "name": "Luca Changretti",
+            "email": "luca@gmail.com",
             "phone": "333-3690",
             "table_size": 10,
-            "date_time": "2025-04-03T23:05:32.826000"
+            "date_time": datetime.fromisoformat("2025-04-03T23:05:32.826000")
         }
     ]
 db = SessionLocal()
