55a29b8485febf464600604593d587b75daab405
alexisalvz3 <alexis.alvz3@gmail.com>
2025-06-12 20:37:37 -0700

    setting up db engine pt.4

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,4 +1,30 @@
-from app.database import engine, Base
+from app.database import SessionLocal, engine, Base
 from app.models import Reservation, NotificationLog, AdminUser  # all your models
 
 Base.metadata.create_all(bind=engine)
+
+reservations = [
+        {
+            "name": "kambala",
+            "phone": "507-2424",
+            "table_size": 10,
+            "date_time": "2025-04-02T19:51:11.161000"
+        },
+        {
+            "name": "Thomas Shelby",
+            "phone": "432-4653",
+            "table_size": 3,
+            "date_time": "2025-04-03T02:18:15.356000"
+        },
+        {
+            "name": "Luca Changretti",
+            "phone": "333-3690",
+            "table_size": 10,
+            "date_time": "2025-04-03T23:05:32.826000"
+        }
+    ]
+db = SessionLocal()
+for r in reservations:
+    db.add(Reservation(**r))
+db.commit()
+db.close()
